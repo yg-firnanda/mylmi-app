44df4e96b5d931de2421f7615fb50e0eb14aeca6
Mochammad Yoga Firnanda <mochyoga475@gmail.com>
2024-03-25 18:58:37 +0700

    Completing Auth API

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            "name" => "required",
+            "email" => "required|unique:users,email",
+            "password" => "required",
+            "confirm_password" => "required|same:password",
+            "birth_date" => "required",
+        ]);
+
+        User::create($validatedData);
+
+        return response()->json([
+            "success" => true,
+            "message" => "Successful register as user",
+            "data" => [
+                "name" => $validatedData['name'],
+                "email" => $validatedData['email'],
+                "birth_date" => $validatedData['birth_date'],
+            ],
+        ], 201);
+    }
+    
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                "success" => false,
+                "message" => "Failed login. Invalid email / password",
+            ], 400);
+        }
+
+        $token = $user->createToken($user->email . "'s authToken")->plainTextToken;
+
+        return response()->json([
+            "success" => true,
+            "message" => "Successful Login",
+            "data" => [
+                "name" => $user->name,
+                "email" => $user->email,
+                "token" => $token,
+            ],
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            return response()->json([
+                "success" => false,
+                "message" => "Unauthenticated",
+            ], 401);
+        }
+        
+        $user->tokens()->delete();
+
+        return response()->json([
+            "success" => true,
+            "message" => "Successful logout",
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,22 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\MagazineController;
 use App\Http\Controllers\API\FundraisingController;
-use App\Http\Controllers\API\Auth\LoginController;
-use App\Http\Controllers\API\Auth\RegisterController;
+
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user()->name('login');
 // })->middleware('auth:sanctum');
 // Auth Routes 
-Route::post('/register', [RegisterController::class, 'store'])->name('auth.register');
-Route::post('/login', [LoginController::class, 'authenticate'])->name('auth.login');
-Route::post('/logout', [LoginController::class, 'logout'])->name('auth.logout');
+Route::post('/register', [AuthController::class, 'store'])->name('auth.register');
+Route::post('/login', [AuthController::class, 'authenticate'])->name('auth.login');
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('/fundraisings', [FundraisingController::class, 'index'])->name('fundraising.index');
     Route::get('/fundraisings/{fundraising:slug}', [FundraisingController::class, 'show'])->name('fundraising.show');
     Route::get('/magazines', [MagazineController::class, 'index'])->name('magazine.index');
     Route::get('/magazines/:slug', [MagazineController::class, 'index'])->name('magazine.show');
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 });
